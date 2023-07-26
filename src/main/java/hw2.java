42df56376329d8bb623b4baba0dc20b51c0f89d7
roman lepihov <lepihovrom@yandex.by>
2023-07-26 11:26:10 +0300

    [update] hw2

--- src/main/java/hw2.java
+++ src/main/java/hw2.java
@@ -4,8 +4,8 @@ public class hw2 {
     public static void main(String[] args) {
 //        task1();
 //        task2();
-//        task3();
-        task4();
+        task3();
+//        task4();
     }
 
     public static Float task1() {
@@ -35,7 +35,10 @@ public class hw2 {
 
             double catchedRes1 = intArray[8] / d;
             System.out.println("catchedRes1 = " + catchedRes1);
-        } catch (ArithmeticException e) {
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (ArithmeticException e) {
             System.out.println("Catching exception: " + e);
         }
     }
@@ -52,9 +55,10 @@ public class hw2 {
 //            System.out.println("Указатель не может указывать на null!");
         } catch (IndexOutOfBoundsException ex) {
             System.out.println("Массив выходит за пределы своего размера!");
-        } catch (Throwable ex) {
-            System.out.println("Что-то пошло не так...");
         }
+//        } catch (Throwable ex) {
+//            System.out.println("Что-то пошло не так...");
+//        }
     }
 
     public static void printSum(Integer a, Integer b) {
